adaac74b18d120d90f573d71ca60bf5f62a05adf
Risshi25 <risshirajsen@gmail.com>
2026-06-23 22:27:45 +0530

    Deploy Next.js out directory to master

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-  output: 'export', // Enable static exports
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  // Disable server components since we're using static export
-};
-
-export default nextConfig;
